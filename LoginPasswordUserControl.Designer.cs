caf15d29f2a681043d4036358608ac73cb453a8c
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-07-21 16:05:44 -0600

    Add project files.

---
+++ LoginPasswordUserControl.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace LoginPasswordUserControl
+{
+    partial class LoginPasswordUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Location = new System.Drawing.Point(59, 52);
+            this.loginLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(63, 25);
+            this.loginLabel.TabIndex = 0;
+            this.loginLabel.Text = "Login:";
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(131, 49);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.ReadOnly = true;
+            this.loginTextBox.Size = new System.Drawing.Size(220, 33);
+            this.loginTextBox.TabIndex = 1;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(131, 111);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.ReadOnly = true;
+            this.passwordTextBox.Size = new System.Drawing.Size(220, 33);
+            this.passwordTextBox.TabIndex = 3;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(27, 114);
+            this.passwordLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.passwordLabel.Size = new System.Drawing.Size(95, 25);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // LoginPasswordUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.loginLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "LoginPasswordUserControl";
+            this.Size = new System.Drawing.Size(425, 313);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label passwordLabel;
+    }
+}
